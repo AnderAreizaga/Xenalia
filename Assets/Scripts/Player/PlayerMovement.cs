cf67937342917f049279757fa4a156fcf0f88a19
OPENDEUSTO\ander.areizagab <aareizaga@euro-help.es>
2018-02-14 13:01:00 +0100

    First commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    Vector2 input;
+    bool isMoving = false;
+    Vector3 startPos;
+    Vector3 endPos;
+    float t;
+
+    private Animator animator;
+
+    public float defWalkSpeed = 4f;
+    private float walkSpeed = 4f;
+
+    public bool isAllowedToMove = true;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        startPos = transform.position;
+        isAllowedToMove = true;
+        animator = GetComponent<Animator>();
+
+        /*rb2d = GetComponent<Rigidbody2D>();*/
+
+    }
+
+    private void FixedUpdate()
+    {
+        float lastInputX = Input.GetAxisRaw("Horizontal");
+        float lastInputY = Input.GetAxisRaw("Vertical");
+        bool running = Input.GetKey("left shift");
+
+        if (lastInputX != 0 || lastInputY != 0)
+        {
+            animator.SetBool("Walking", true);
+            if (running){
+                animator.SetBool("Running", true);
+            }
+            else
+            {
+                animator.SetBool("Running", false);
+            }
+            
+            if(lastInputX > 0)
+            {
+                animator.SetFloat("LastMoveX", 1f);
+            }
+            else if (lastInputX < 0)
+            {
+                animator.SetFloat("LastMoveX", -1f);
+            }
+            else
+            {
+                animator.SetFloat("LastMoveX", 0f);
+            }
+
+            if (lastInputY > 0)
+            {
+                animator.SetFloat("LastMoveY", 1f);
+            }
+            else if (lastInputY < 0)
+            {
+                animator.SetFloat("LastMoveY", -1f);
+            }
+            else
+            {
+                animator.SetFloat("LastMoveY", 0f);
+            }
+        }
+        else
+        {
+
+            animator.SetBool("Walking", false);
+            animator.SetBool("Running", false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+        bool run = Input.GetKey("left shift");
+
+        if (!isMoving && isAllowedToMove)
+        {
+            input = new Vector2(inputX, inputY);
+            if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+                input.y = 0;
+            else
+                input.x = 0;
+
+            if (input != Vector2.zero)
+            {
+                animator.SetFloat("SpeedX", inputX);
+                animator.SetFloat("SpeedY", inputY);
+                if (run)
+                {
+                    walkSpeed = defWalkSpeed * 2;
+                }
+                else
+                {
+                    walkSpeed = defWalkSpeed;
+                }
+                StartCoroutine(Move(transform));
+            }
+
+        }
+    }
+
+    public IEnumerator Move(Transform entity)
+    {
+        isMoving = true;
+        startPos = entity.position;
+        t = 0;
+
+        endPos = new Vector3(startPos.x + System.Math.Sign(input.x), startPos.y + System.Math.Sign(input.y), startPos.z);
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * walkSpeed;
+            entity.position = Vector3.Lerp(startPos, endPos, t);
+            yield return null;
+        }
+
+        isMoving = false;
+        yield return 0;
+    }
+}
