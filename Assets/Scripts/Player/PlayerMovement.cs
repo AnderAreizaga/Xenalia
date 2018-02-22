6a49216ca999ab72b7e878859e97c66464dddaba
AnderAreizaga <ander.areizagab@opendeusto.es>
2018-02-22 13:49:31 +0100

    collisions and movement fix

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     Vector2 input;
     bool isMoving = false;
-    Vector3 startPos;
-    Vector3 endPos;
+    Vector2 startPos;
+    Vector2 endPos;
     float t;
 
     private Animator animator;
@@ -23,11 +23,50 @@ public class PlayerMovement : MonoBehaviour {
         isAllowedToMove = true;
         animator = GetComponent<Animator>();
 
-        /*rb2d = GetComponent<Rigidbody2D>();*/
+        rb2d = GetComponent<Rigidbody2D>();
 
     }
 
     private void FixedUpdate()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+        bool run = Input.GetKey("left shift");
+
+        if (!isMoving && isAllowedToMove)
+        {
+            input = new Vector2(inputX, inputY);
+            if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+                input.y = 0;
+            else
+                input.x = 0;
+
+            if (input != Vector2.zero)
+            {
+                animator.SetFloat("SpeedX", inputX);
+                animator.SetFloat("SpeedY", inputY);
+                if (run)
+                {
+                    walkSpeed = defWalkSpeed * 2;
+                }
+                else
+                {
+                    
+                    walkSpeed = defWalkSpeed;
+                }
+                rb2d.velocity = input * walkSpeed;
+            }
+            else
+            {
+                rb2d.velocity = input * 0f;
+            }
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         float lastInputX = Input.GetAxisRaw("Horizontal");
         float lastInputY = Input.GetAxisRaw("Vertical");
@@ -36,15 +75,16 @@ public class PlayerMovement : MonoBehaviour {
         if (lastInputX != 0 || lastInputY != 0)
         {
             animator.SetBool("Walking", true);
-            if (running){
+            if (running)
+            {
                 animator.SetBool("Running", true);
             }
             else
             {
                 animator.SetBool("Running", false);
             }
-            
-            if(lastInputX > 0)
+
+            if (lastInputX > 0)
             {
                 animator.SetFloat("LastMoveX", 1f);
             }
@@ -78,51 +118,19 @@ public class PlayerMovement : MonoBehaviour {
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float inputY = Input.GetAxisRaw("Vertical");
-        bool run = Input.GetKey("left shift");
-
-        if (!isMoving && isAllowedToMove)
-        {
-            input = new Vector2(inputX, inputY);
-            if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
-                input.y = 0;
-            else
-                input.x = 0;
-
-            if (input != Vector2.zero)
-            {
-                animator.SetFloat("SpeedX", inputX);
-                animator.SetFloat("SpeedY", inputY);
-                if (run)
-                {
-                    walkSpeed = defWalkSpeed * 2;
-                }
-                else
-                {
-                    walkSpeed = defWalkSpeed;
-                }
-                StartCoroutine(Move(transform));
-            }
-
-        }
-    }
-
     public IEnumerator Move(Transform entity)
     {
         isMoving = true;
         startPos = entity.position;
         t = 0;
 
-        endPos = new Vector3(startPos.x + System.Math.Sign(input.x), startPos.y + System.Math.Sign(input.y), startPos.z);
+        endPos = new Vector2(startPos.x + System.Math.Sign(input.x), startPos.y + System.Math.Sign(input.y));
 
         while (t < 1f)
         {
             t += Time.deltaTime * walkSpeed;
-            entity.position = Vector3.Lerp(startPos, endPos, t);
+            
+            rb2d.velocity = endPos * t;
             yield return null;
         }
 
