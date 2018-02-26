a7e7d150fd2f5513a463faa23da68c95addf1ac8
OPENDEUSTO\ander.areizagab <aareizaga@euro-help.es>
2018-02-26 13:52:19 +0100

    UI start

---
+++ Assets/Scripts/TrainerInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainerInfo : MonoBehaviour {
+
+    //Clase del entrenador
+    public enum Clase
+    {
+        EntrenadorGuay,
+        Karateka
+    };
+
+    private static string[] nombreClase = new string[]
+   {
+        "Entrenador Guay",
+        "Karateka"
+   };
+
+    private static int[] recompensaClase = new int[]
+    {
+        100,
+        60
+    };
+}
